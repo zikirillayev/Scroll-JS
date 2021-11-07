7e3c8cd9e7b8a6cfde0b3ec6c783c86db55bdf28
zikirillayev <91782186+zikirillayev@users.noreply.github.com>
2021-11-07 11:50:09 +0500

    Add files via upload

---
+++ init.js
@@ -0,0 +1,30 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+M.AutoInit()
+
+
+// let s = documetn.getElementById("select");
+// s.onchange = ()=>{
+//   let val = s.value;
+//   console.log(val)
+//   if (val === "triangle"){
+//     document.querySelector(".triangle").style.display = "block"  
+//   }else{
+//     document.querySelector(".triangle").style.display = "none"  
+//   }
+//   if (val === "react"){
+//     document.querySelector(".rect").style.display = "block"  
+//   }else{
+//     document.querySelector(".rect").style.display = "none"  
+//   }
+//   if (val === "circle"){
+//     document.querySelector(".circle").style.display = "block"  
+//   }else{
+//     document.querySelector(".circle").style.display = "none"  
+//   }
+// }
